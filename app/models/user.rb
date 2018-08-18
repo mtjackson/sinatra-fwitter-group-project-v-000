c291cd076b2da2f0b1e57893fe4adfd23be3cea4
Mary Jackson <jackson.marytherese@gmail.com>
2018-08-18 13:50:27 +0000

    Done.

---
+++ app/models/user.rb
@@ -0,0 +1,15 @@
+class User < ActiveRecord::Base
+  has_many :tweets
+
+  def slug
+    self.name.parameterize
+  end
+
+  def self.find_by_slug(slug)
+    self.all.find do |user|
+      if slug == user.slug
+        user
+      end
+    end
+  end
+end
