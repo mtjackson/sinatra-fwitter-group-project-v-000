0f48abcc66a81ae8465d5ac5d98b8edb18968d59
Mary Jackson <jackson.marytherese@gmail.com>
2018-08-18 15:43:04 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,8 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "password_security"
   end
 
   get '/' do
@@ -12,7 +14,18 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/signup' do
-    erb :create_user
+    erb :'users/create_user'
+  end
+
+  post '/signup' do
+    @user = User.create(params[:user])
+    if @user.save && @user.username != ""
+      redirect to "tweets/tweets"
+    end
+  end
+
+  get 'tweets/tweets' do
+    erb :'tweets/tweets'
   end
 
 end
