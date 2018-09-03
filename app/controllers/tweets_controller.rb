5e6fe5f4e932147e5885902db890fd2b0df503cc
Mary Jackson <jackson.marytherese@gmail.com>
2018-09-03 18:43:41 +0000

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -9,4 +9,24 @@ class TweetsController < ApplicationController
     end
   end
 
+  get '/tweets/new' do
+    if logged_in?
+      erb :'tweets/create_tweet'
+    else
+      redirect to '/login'
+    end
+  end
+
+  post '/tweets/new' do
+    @user = User.find(session[:user_id])
+    if params[:content]
+      @tweet = Tweet.create(:content => params[:content], :user_id => @user.id)
+      # binding.pry
+      redirect to '/tweets'
+    else
+      redirect to '/tweets/new'
+    end
+  end
+
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,18 +29,18 @@ class UsersController < ApplicationController
 
   post '/login' do
     @user = User.find_by(:username => params[:username])
-    if @user && user.authenticate(params[:password])
+    if @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id
-      redirect to '/tweets'
+      redirect to "/tweets"
     else
-      redirect to '/signup'
+      redirect to "/signup"
     end
   end
 
   get '/logout' do
     if logged_in?
       session.clear
-      redirect to '/'
+      redirect to '/login'
     else
       redirect to '/login'
     end
