61b08572512a4b80bf63e00e8a8c239d3002d2e1
Mary Jackson <jackson.marytherese@gmail.com>
2018-09-03 15:54:12 +0000

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,4 +27,28 @@ class UsersController < ApplicationController
     end
   end
 
+  post '/login' do
+    @user = User.find_by(:username => params[:username])
+    if @user && user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect to '/tweets'
+    else
+      redirect to '/signup'
+    end
+  end
+
+  get '/logout' do
+    if logged_in?
+      session.clear
+      redirect to '/'
+    else
+      redirect to '/login'
+    end
+  end
+
+  get "/users/:slug" do
+    @user = User.find_by_slug(params(:slug))
+    erb :'users/show'
+  end
+
 end
