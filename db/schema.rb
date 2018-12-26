8fce193cb99c08852593a61fb9a6ea3176c3cfa4
Mary Jackson <jackson.marytherese@gmail.com>
2018-12-26 17:54:17 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181226175038) do
+ActiveRecord::Schema.define(version: 20181226175347) do
+
+  create_table "tweets", force: :cascade do |t|
+    t.string  "content"
+    t.integer "user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
